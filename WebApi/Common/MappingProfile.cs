c7ec4d25432bce5932e054843f5b04a888f79765
onur kuş <onurkus74@gmail.com>
2022-04-17 16:19:39 +0300

    Actor and customer mapping

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -3,13 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Applications.ActorOperations.Commands.CreateActor;
 using WebApi.Applications.ActorOperations.Queries.GetActors;
+using WebApi.Applications.CustomerOperations.Commands.CreateCustomer;
 using WebApi.Applications.DirectorOperations.Commands.CreateDirector;
 using WebApi.Applications.DirectorOperations.Queries.GetDirectorDetail;
 using WebApi.Applications.DirectorOperations.Queries.GetDirectors;
 using WebApi.Applications.MovieOperations.Commands.CreateMovie;
 using WebApi.Applications.MovieOperations.Queries.GetMovies;
 using WebApi.Entities;
+using static WebApi.Applications.ActorOperations.Queries.GetActorDetail.GetActorDetailQuery;
 using static WebApi.Applications.MovieOperations.Queries.GetMovieDetail.GetMovieDetailQuery;
 
 namespace WebApi.Common
@@ -42,9 +45,18 @@ namespace WebApi.Common
 
             CreateMap<CreateDirectorModel, Director>();
 
-            //Actor
+            //ACTOR
             CreateMap<Actor, ActorViewModel>()
                 .ForMember(dest => dest.MovieActors, opt => opt.MapFrom(src => src.MovieActors.Select(x => x.Movie.Name)));
+
+            CreateMap<Actor, ActorDetailViewModel>()
+                .ForMember(dest => dest.MovieActors, opt => opt.MapFrom(src => src.MovieActors.Select(x => x.Movie.Name)));
+
+            CreateMap<CreateActorCommand, Actor>();
+
+            //CUSTOMER
+
+            CreateMap<CreateCustomerCommand, Customer>();
                  
         }
     }
