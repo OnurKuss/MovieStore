a20c50df6c6aa169fb8a3c9d7e8f432ab41522c2
onur kuş <onurkus74@gmail.com>
2022-04-17 16:18:59 +0300

    Actor Controller added

--- WebApi/Controllers/ActorController.cs
+++ WebApi/Controllers/ActorController.cs
@@ -1,10 +1,15 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Applications.ActorOperations.Commands.CreateActor;
+using WebApi.Applications.ActorOperations.Commands.DeleteActor;
+using WebApi.Applications.ActorOperations.Commands.UpdateActor;
+using WebApi.Applications.ActorOperations.Queries.GetActorDetail;
 using WebApi.Applications.ActorOperations.Queries.GetActors;
 using WebApi.DbOperations;
 
@@ -27,6 +32,50 @@ namespace WebApi.Controllers
         {
             GetActorsQuery query = new GetActorsQuery(_context, _mapper);
             return Ok(query.Handle());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetActorDetails(int id)
+        {
+            GetActorDetailQuery query = new GetActorDetailQuery(_context, _mapper);
+            query.ActorId = id;
+            GetActorDetailQueryValidator validator = new GetActorDetailQueryValidator();
+            validator.ValidateAndThrow(query);
+            return Ok(query.Handle());
+        }
+
+        [HttpPost]
+        public IActionResult AddActor([FromBody] CreateActorModel model)
+        {
+            CreateActorCommand command = new CreateActorCommand(_context,_mapper);
+            command.Model = model;
+            CreateActorCommandValidator validator = new CreateActorCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle();
+            return Ok("Aktör başarı ile eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteActor(int id)
+        {
+            DeleteActorCommand command = new DeleteActorCommand(_context);
+            command.ActorId = id;
+            DeleteActorCommandValidator validator = new DeleteActorCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle();
+            return Ok("Aktör başarı ile silindi!!");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateActor(int id, [FromBody] UpdateActorModel model)
+        {
+            UpdateActorCommand command = new UpdateActorCommand(_context);
+            command.ActorId = id;
+            command.Model = model;
+            UpdateActorCommandValidator validator = new UpdateActorCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle();
+            return Ok("Aktör başarı ile güncellendi!!");
         }
     }
 }
